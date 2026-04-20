8d18b1b92f146104a8ff523b7390bd2a36b0a43d
doantien03 <doantienle461@gmail.com>
2026-04-20 23:41:08 +0700

    update headerComponent

--- frontend/js/components/headerComponent.js
+++ frontend/js/components/headerComponent.js
@@ -8,11 +8,33 @@ export function renderHeader() {
       <div class="logo">FASHION</div>
 
       <nav>
-        <a href="../pages/home.html" class="nav-link">TRANG CHỦ</a>                
+        <a href="../pages/home.html" class="nav-link">TRANG CHỦ</a>
+        
+        <div class="nav-item">
         <a href="/ao" class="nav-link">ÁO</a>
+        <div class="dropdown">
+            <a href="/ao/phong">Áo Phông</a>
+            <a href="/ao/polo">Áo Polo</a>
+            <a href="/ao/somi">Áo Sơ Mi</a>
+        </div></div>
+        
+        <div class="nav-item">
         <a href="/quan" class="nav-link">QUẦN</a>
+        <div class="dropdown">
+            <a href="/quan/dai">Quần dài</a>
+            <a href="/quan/short">Quần short</a>
+        </div></div>
+        
+        <div class="nav-item">
         <a href="/phu-kien" class="nav-link">PHỤ KIỆN</a>
+        <div class="dropdown">
+            <a href="/phu-kien/tui-balo">Túi/Balo</a>
+            <a href="/phu-kien/giay-dep">Giày dép</a>
+            <a href="/phu-kien/day-lung">Dây lưng</a>
+          </div>
+        </div>
         <a href="/thong-tin" class="nav-link">THÔNG TIN</a>
+        
       </nav>
 
       <div class="icons">
