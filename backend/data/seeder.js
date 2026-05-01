813cabebf3fb0c1ee971e9e6a1939363e1b5ec5f
doantien03 <doantienle461@gmail.com>
2026-05-01 18:47:47 +0700

    feat: update product data and home page

--- backend/data/seeder.js
+++ backend/data/seeder.js
@@ -1,21 +1,26 @@
 const mongoose = require("mongoose");
 const Product = require("../models/product");
 const products = require("./products.json");
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config();
 
 console.log("MONGO_URI:", process.env.MONGO_URI);
 
 const seedProducts = async () => {
   try {
+    // Kết nối DB
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Kết nối MongoDB thành công!");
 
-    await Product.deleteMany();
-    console.log("Đã xóa data cũ!");
+    // Lặp từng sản phẩm
+    for (const item of products) {
+      await Product.updateOne(
+        { name: item.name },   
+        { $set: item },       
+        { upsert: true }      
+      );
+    }
 
-    await Product.insertMany(products);
-    console.log("Import sản phẩm thành công!");
-
+    console.log("Thêm / cập nhật sản phẩm thành công!");
     process.exit();
 
   } catch (err) {
